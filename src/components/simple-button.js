ce522d48bb2e855817a1f1ef2cd660eeb2496e5d
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-07-19 19:57:54 -0500

    💄 added styles

--- src/components/simple-button.js
+++ src/components/simple-button.js
@@ -1,10 +1,30 @@
-import { html, LitElement } from 'lit-element'
+import { html, css, LitElement } from 'lit-element'
 
 class Button extends LitElement {
 	static get properties() {
 		return {
 			handleClick: { type: Function },
 		}
+	}
+
+	static get styles() {
+		return css`
+			button {
+				padding: 10px 25px;
+				border-radius: 5px;
+				transition: all 0.2s ease-in-out;
+				box-shadow: 4px 5px 0px 0px #1e3a8a;
+				cursor: pointer;
+				font-size: 1.5rem;
+				border: none;
+				background-color: #93c5fd;
+				color: #1e3a8a;
+			}
+
+			button:hover {
+				box-shadow: none;
+			}
+		`
 	}
 
 	constructor() {
