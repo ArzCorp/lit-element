1634d3626ee69b2226e93ef27d38cbfdcd7a3928
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-07-29 23:12:21 -0500

    💄 added input styles

--- src/components/app-input.js
+++ src/components/app-input.js
@@ -1,8 +1,56 @@
-import { LitElement, html } from 'lit-element'
+import { LitElement, html, css } from 'lit-element'
 
 class AppInput extends LitElement {
+	static get properties() {
+		return {
+			label: { type: String },
+			name: { type: String },
+			id: { type: String },
+			value: { type: String },
+			error: { type: String },
+			handleChange: { type: Function },
+		}
+	}
+
+	static get styles() {
+		return css`
+			:host {
+				color: var(--color-primary-dark);
+				display: block;
+				box-sizing: border-box;
+				font-size: 1.6rem;
+			}
+
+			label {
+				display: block;
+				margin-bottom: 0.5rem;
+				font-size: 1.4rem;
+				font-weight: medium;
+			}
+
+			input {
+				color: var(--color-dark);
+				border: 1px solid var(--color-primary);
+				border-radius: 0.5rem;
+				padding: 0.7rem;
+				outline-color: var(--color-primary-light);
+			}
+		`
+	}
+
 	render() {
-		return html`<input type="text" name="" id="" />`
+		return html`
+			<label htmlFor="${this.name}">${this.label}</label>
+			<input
+				type="text"
+				name="${this.name}"
+				id="${this.id}"
+				value="${this.value}"
+				onkeyup="${this.handleChange}"
+			/>
+			<p>${this.error}</p>
+		`
 	}
 }
+
 customElements.define('app-input', AppInput)
