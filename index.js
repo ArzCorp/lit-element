b56be9d321a7da5ff25c6ac7b5e883c48b1b9619
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-07-31 20:15:28 -0500

    🔥 removed usless files

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-import './src/components/ghost-button.js'
-import './src/components/regular-button.js'
-import './src/components/app-header.js'
-import './src/components/app-notification.js'
 import './src/components/my-component.js'
 import './src/components/app-input.js'
