9dbb0dfb6151f038eb8ac6c433a23b402a52353b
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-08-02 22:43:39 -0500

    💄 added slotted styles

--- src/components/my-component.js
+++ src/components/my-component.js
@@ -14,25 +14,13 @@ class MyComponent extends LitElement {
 				width: 210px;
 			}
 
-			::slotted(*) {
-				color: var(--color-primary);
-			}
-
-			::slotted(p) {
-				font-size: 1.4rem;
-			}
-
-			span ::slotted(*) {
-				color: var(--color-error);
-			}
-
 			div {
 				display: block;
 				min-height: 250px;
 				height: inherit;
 				background-color: var(--color-white);
 				padding: 1rem;
-				border: 1px solid var(--color-primary);
+				border: 1px solid var(--component-color, #1e3a8a);
 				border-radius: 0.6rem;
 			}
 
@@ -40,6 +28,23 @@ class MyComponent extends LitElement {
 				font-size: 1rem;
 				text-align: right;
 				font-weight: bold;
+				color: var(--component-color, #1e3a8a);
+			}
+
+			::slotted(*) {
+				color: var(--component-color, #1e3a8a);
+			}
+
+			::slotted(p) {
+				font-size: 1.4rem;
+			}
+
+			::slotted(h1) {
+				color: var(--color-primary);
+			}
+
+			span ::slotted(*) {
+				color: var(--color-error);
 			}
 		`
 	}
